e5da322359101ad0c7a2fc5eb90a6a676e4a43ad
KilJae69 <adi.toromanovic@outlook.com>
2025-01-10 12:46:02 +0100

    .3d commit

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+import { Content, KeyTextField } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import { gsap } from "gsap";
+import Bounded from "@/components/Bounded";
+import { Shapes } from "./Shapes";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        ".name-animation",
+        { x: -100, opacity: 0, rotate: -10 },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3)",
+          duration: 1,
+          transformOrigin: "left top",
+          stagger: {
+            each: 0.1,
+            from: "random",
+          },
+        }
+      );
+
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          scale: 1,
+          delay: 0.2,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
+    }, component);
+    return () => ctx.revert();
+  }, []);
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
+
+    return name.split("").map((letter, index) => (
+      <span
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+        key={index}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      ref={component}
+    >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
+        <div className="col-start-1 md:row-start-1">
+          <h1
+            aria-label={`${slice.primary.first_name} ${slice.primary.last_name}`}
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter "
+          >
+            <span className="block text-slate-300 whitespace-nowrap	">
+              {renderLetters(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500 whitespace-nowrap	">
+              {renderLetters(slice.primary.last_name, "last")}
+            </span>
+          </h1>
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
+            {slice.primary.tag_line}
+          </span>
+        </div>
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
